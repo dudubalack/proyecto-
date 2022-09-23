c2e2ee356e2f344203af94866700f6eae43a3c64
dudubalack <dominguesdatrindadecarloseduar@gmail.com>
2022-09-23 16:50:02 +0000

    primeiro commit

--- src/api/admin.py
+++ src/api/admin.py
@@ -1,7 +1,7 @@
   
 import os
 from flask_admin import Admin
-from .models import db, User
+from .models import db, User, Profesional_de_la_salud, Rol, Post, Grupo_de_apoio
 from flask_admin.contrib.sqla import ModelView
 
 def setup_admin(app):
@@ -12,6 +12,10 @@ def setup_admin(app):
     
     # Add your models here, for example this is how we add a the User model to the admin
     admin.add_view(ModelView(User, db.session))
+    admin.add_view(ModelView(Rol, db.session))
+    admin.add_view(ModelView(Profesional_de_la_salud, db.session))
+    admin.add_view(ModelView(Grupo_de_apoio, db.session))
+    admin.add_view(ModelView(Post, db.session))
 
     # You can duplicate that line to add mew models
     # admin.add_view(ModelView(YourModelName, db.session))
--- src/api/models.py
+++ src/api/models.py
@@ -6,7 +6,12 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    name = db.Column(db.String(250))
+    last_name = db.Column(db.String(250))
+    rol_id = db.Column(db.Integer,db.ForeignKey('rol.id'))
+    profesional_de_la_salud = db.relationship("Profesional_de_la_salud",backref='user')
+    grupo_de_apoio = db.relationship("Grupo_de_apoio",backref='user')
+    post = db.relationship("Post",backref='user')
 
     def __repr__(self):
         return f'<User {self.email}>'
@@ -17,3 +22,91 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+class Rol (db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    rol = db.Column(db.String(80),unique=True,nullable=False)
+    user = db.relationship('User',backref='rol')
+
+    def __repr__(self):
+        return f'<Rol {self.rol}>'
+
+
+    def serialize(self):
+        return{
+           "id": self.id,
+           "rol": self.rol, 
+        }
+     
+
+
+class Profesional_de_la_salud (db.Model):
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = db.Column(db.Integer, primary_key=True)
+    grado = db.Column(db.String(250),nullable=False)
+    especialidad = db.Column(db.String(250), nullable=False)
+    user_id = db.Column(db.Integer,db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return f'<Profesional_de_la_salud {self.especialidad}>'
+
+
+    def serialize(self):
+        return{
+           "id": self.id,
+           "grado": self.grado, 
+           "especialidad": self.especialidad,
+           "user_id": self.user_id,
+        }
+
+
+class Grupo_de_apoio (db.Model):
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250),nullable=False)
+    descripcion = db.Column(db.String(250),nullable=False)
+    tema = db.Column(db.String(250), nullable=False)
+    user_id = db.Column(db.Integer,db.ForeignKey('user.id'))
+    post = db.relationship('Post')
+
+    def __repr__(self):
+        return f'<Grupo_de_apoio {self.name}>'
+
+
+    def serialize(self):
+        return{
+           "id": self.id,
+           "name": self.name,
+           "descripcion": self.descripcion,
+           "tema": self.tema,
+           "use_id": self.user_id,
+           "post": self.post,
+
+        }
+
+class Post (db.Model):
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer,db.ForeignKey('user.id'))
+    grupo_id = db.Column(db.Integer,db.ForeignKey('grupo_de_apoio.id'))
+    creado = db.Column(db.DateTime(timezone=True))
+    txt = db.Column(db.String(300),nullable=False)
+
+    def __repr__(self):
+        return f'<Post {self.id}>'
+
+
+    def serialize(self):
+        return{
+           "id": self.id,
+           "user_id": self.user_id,
+           "grupo_id": self.grupo_id,
+           "creado": self.creado,
+           "txt": self.txt,
+
+        }
+      
+
+        
